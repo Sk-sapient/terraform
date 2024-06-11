dea7da8aae9785a5a26ab66308a222fcedfcb63d
Sk-sapient <shatrudhnkrsingh95@gmail.com>
2024-06-11 16:51:32 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,39 @@
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
+resource "google_compute_network" "vpc_network" {
+  project                         = var.project
+  name                            = var.network
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+  depends_on = [
+    google_project_service.compute,
+    google_project_service.container
+  ]
+}
+
+resource "google_compute_subnetwork" "private" {
+  name                     = "private"
+  ip_cidr_range            = "10.0.0.0/16"
+  region                   = var.region
+  network                  = google_compute_network.vpc_network.id
+  private_ip_google_access = true
+
+  secondary_ip_range {
+    range_name    = "k8s-pod-range"
+    ip_cidr_range = "10.10.0.0/14"
+  }
+
+  secondary_ip_range {
+    range_name    = "k8s-service-range"
+    ip_cidr_range = "10.20.0.0/20"
+  }
+}
