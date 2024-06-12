8155a6dc652e6c58f6f76783fef24fc861ceacaf
Sk-sapient <shatrudhnkrsingh95@gmail.com>
2024-06-12 12:22:29 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -37,7 +37,7 @@ resource "google_container_cluster" "primary" {
   }
 
   workload_identity_config {
-    workload_pool = "burner-dhijha.svc.id.goog"
+    workload_pool = "burner-kumshatr.svc.id.goog"
   }
 
   ip_allocation_policy {
